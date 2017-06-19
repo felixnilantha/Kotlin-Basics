5444df6e82bc3d8f8597ff57ea0e0e23bd9a78fc
Nilantha Silva <felixnilantha@gmail.com>
2017-06-19 16:24:22 +1000

    factorial : initial code

---
+++ src/org/kotlinbasics/Factorial.kt
@@ -0,0 +1,22 @@
+package org.kotlinbasics
+
+/* to get a better understanding about factorial please refere to the wiki link
+ 	https://en.wikipedia.org/wiki/Factorial
+ */
+
+fun main(args: Array<String>) {
+	println(factorial(0))
+	println(factorial(1))
+	println(factorial(2))
+	println(factorial(3))
+	println(factorial(4))
+	println(factorial(5))
+}
+
+fun factorial(n: Int): Int{
+	if (n <= 1){
+		return 1
+	}else{
+		return factorial(n-1) * n
+	}
+}
