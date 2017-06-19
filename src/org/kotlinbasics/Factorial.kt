78bacb261f66e1c3f3e20d3cb85de6b968a2707a
Nilantha Silva <felixnilantha@gmail.com>
2017-06-19 16:32:19 +1000

    factorial: using when statement instead of if statement

--- src/org/kotlinbasics/Factorial.kt
+++ src/org/kotlinbasics/Factorial.kt
@@ -14,9 +14,8 @@ fun main(args: Array<String>) {
 }
 
 fun factorial(n: Int): Int {
-	return if (n <= 1) {
-		1
-	} else {
-		factorial(n - 1) * n
+	return when {
+		n <= 1 -> 1
+		else -> factorial(n - 1) * n
 	}
 }
