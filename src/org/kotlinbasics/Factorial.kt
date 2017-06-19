31ee0645721c18d19f8b1405161fea48365e4734
Nilantha Silva <felixnilantha@gmail.com>
2017-06-19 22:46:47 +1000

    Factorial : Test Factorial

--- src/org/kotlinbasics/Factorial.kt
+++ src/org/kotlinbasics/Factorial.kt
@@ -1,21 +1,29 @@
 package org.kotlinbasics
 
+import org.junit.Test
+import org.hamcrest.MatcherAssert.assertThat
+import org.hamcrest.CoreMatchers.equalTo
+
 /* to get a better understanding about factorial please refere to the wiki link
  	https://en.wikipedia.org/wiki/Factorial
  */
+class TestFactorial {
+	
 
-fun main(args: Array<String>) {
-	println(factorial(0))
-	println(factorial(1))
-	println(factorial(2))
-	println(factorial(3))
-	println(factorial(4))
-	println(factorial(5))
+	@Test fun calculateFactorial() {
+
+		assertThat(factorial(0), equalTo(1))
+		assertThat(factorial(1), equalTo(1))
+		assertThat(factorial(2), equalTo(2))
+		assertThat(factorial(3), equalTo(6))
+		assertThat(factorial(4), equalTo(24))
+		assertThat(factorial(5), equalTo(120))
+	}
 }
 
 
 // inline exaample 
-fun factorial(n: Int): Int{
-	return if (n <= 1) 1 else factorial(n-1) * n
-	
+fun factorial(n: Int): Int {
+	return if (n <= 1) 1 else factorial(n - 1) * n
+
 }
